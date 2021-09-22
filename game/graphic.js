da3c3254bdea73a99df82122b5e05de066595d3c
Martin Coulon <martin.coulon@epita.fr>
2021-09-22 21:45:37 +0200

    Fix ground and light

--- game/graphic.js
+++ game/graphic.js
@@ -24,9 +24,12 @@ function init()
 
     noGround = [];
     ground = new Ground(0xffffff, WIDTH, HEIGHT, 10);
-    
+
     player1 = new Player("player1", 0xffff00, new THREE.Vector2(50, 0), 0);
     scene.add(player1.graphic);
+
+    ennemy1 = new Ennemy("ennemy1",0xffff00, new THREE.Vector2(50, 0), 0);
+    scene.add(ennemy1.graphic);
 
     light1 = new Light("sun", 0xffffff, "0,0,340");
     scene.add(light1);
@@ -48,7 +51,6 @@ function Ground(color, size_x, size_y, nb_tile)
         for (y = minY; y <= maxY; y = y+sizeOfTileY){
 
             color = colors[Math.floor(Math.random()*colors.length)];
-       
             if (0x000000 != color)
             {
                 tmpGround = new THREE.Mesh(
@@ -59,14 +61,19 @@ function Ground(color, size_x, size_y, nb_tile)
                 scene.add(tmpGround);
             }
             else
-                noGround.push([x, y]);
+            {
+                if (x !=0 && y != 0)
+                    noGround.push([x, y]);
+                else
+                    color = 0xff0000;
+            }
         }
     }
 }
 
 function Light(name, color, position)
 {
-    pointLight = new THREE.PointLight(color, 50, 350);
+    pointLight = new THREE.PointLight(color, 50, 1000);
 
     pointLight.position.x = position.split(',')[0];
     pointLight.position.y = position.split(',')[1];
